131c56b1f39808ca0e31a47bf515804c70a6f343
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2017-03-10 01:01:24 +0530

    Add files via upload

--- todo/todo.py
+++ todo/todo.py
@@ -5,6 +5,27 @@ import datetime
 import io
 from bottle import run, route
 
+page = str('''
+<html>
+<head>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Quicksand');
+</style> 
+</head>
+<body style = "padding: 0; Margin: 0; font-family: 'Quicksand', sans-serif;">
+<h1>{}</h1>
+<h2>Today</h2>
+<ul>
+{}
+</ul>
+<h2>Tomorrow</h2>
+<ul>
+{}
+</ul>
+</body>
+</html>
+''')
+
 try:
     to_unicode = unicode
 except NameError:
@@ -25,8 +46,14 @@ try:
 			#add an item
 			print("{} added to {}".format(sys.argv[2], sys.argv[3]))
 			with open(sys.argv[3] + '.json') as data_file:    
-    				item = json.load(data_file)
-			item.update({sys.argv[2] : {'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}})
+				item = json.load(data_file)
+			if (sys.argv[2] not in item.keys()):
+				item.update({sys.argv[2] : {'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'target': sys.argv[4] + ' ' + sys.argv[5], 'desc': ' '.join(sys.argv[6:])}})
+			else:
+				if sys.argv[4] != item[sys.argv[2]]['target'].split()[1] or sys.argv[5] != item[sys.argv[2]]['target'].split()[-1]:
+					item.update({sys.argv[2] : {'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'target': sys.argv[4] + ' ' + sys.argv[5], 'desc': ' '.join(sys.argv[6:])}})
+				else:
+					print('The item cannot be updated')
 			with io.open(sys.argv[3] + '.json', 'w', encoding='utf8') as outfile:
     				str_ = json.dumps(item, indent=4, sort_keys=True, separators=(',', ':'), ensure_ascii=False)
     				outfile.write(to_unicode(str_))
@@ -41,8 +68,21 @@ try:
 		else:
 			print("There is no such user!!!")
 	
-	#elif( sys.argv[1] == 'delete' ):
-		#print("delete")
+	elif( sys.argv[1] == 'delete' ):
+		print('successfully deleted')
+		os.remove(sys.argv[2] + '.json')
+	elif( sys.argv[1] == 'remove' ):
+		if os.path.exists(sys.argv[3] + '.json'):
+			print('successfully removed {} from {}'.format(sys.argv[2], sys.argv[3]))
+			with open(sys.argv[3] + '.json') as data_file:
+					d = json.load(data_file)
+			del d[sys.argv[2]]
+			open(sys.argv[3] + '.json', 'w').write('')
+			with io.open(sys.argv[3] + '.json', 'w', encoding='utf8') as outfile:
+    				outfile.write(to_unicode(json.dumps(d, indent=4, sort_keys=True, separators=(',', ':'), ensure_ascii=False)))
+		else:
+			print("There is no such user!!!")
+
 	elif( sys.argv[1] == 'show' ):
 		@route('/show/<name>')
 		def show(name):
@@ -50,8 +90,8 @@ try:
 				name = sys.argv[2]
 				with open(sys.argv[2] + '.json') as data_file:
 					d = json.load(data_file)
-				ites = ['<li>{}</li>'.format(i) for i in d.keys()]
-				return ''.join(ites)
+				ites = ''.join(['<li>{}</li>'.format(i) for i in d.keys()])
+				return page.format(sys.argv[2], ites, ites)
 		run(reload = True, debug = True)
 
 	else:
